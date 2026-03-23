30ebaae73697f487365c7dd4ad8ff13e5d0cb008
Bhavy <bhavyajain817@gmail.com>
2026-03-23 17:50:21 +0530

    changes

--- src/app/api/bulk-demand-pdf/route.ts
+++ src/app/api/bulk-demand-pdf/route.ts
@@ -100,7 +100,6 @@ export async function POST(request: NextRequest) {
                 defaultViewport: chromium.defaultViewport,
                 executablePath: await chromium.executablePath(),
                 headless: chromium.headless,
-                ignoreHTTPSErrors: true,
             });
         }
 
